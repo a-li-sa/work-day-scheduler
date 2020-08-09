5900f47fdd38788c45b795a2bbf0e26233e0d992
Alisa Poon <alisa.poon97@gmail.com>
2020-08-09 15:19:22 -0700

    change hour to show AM/PM

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
+console.log(moment().format('LT'))
 $(document).ready(function() {
-  for (let i = 0; i < 8; i++) {
-    const $container = $('.container');
-    const $timeBlockDiv = $('<div>').addClass("time-block row");
-    let $hourDiv = $('<div>').addClass("text-right col-1 hour").text('\n' + (9+i));
-    let $textArea = $('<textarea>').addClass("col").attr("time-value", (9+i));
-    if ($textArea.attr("time-value") < moment().hour()) {
-      $textArea.addClass("past");
-    } else if ($textArea.attr("time-value") > moment().hour()) {
-      $textArea.addClass("future");
-    } else {
-      $textArea.addClass("present");
-    }
-    let $button = $('<button>').addClass("col-1 saveBtn");
-    const $saveIcon = $('<i>').addClass("fas fa-save");
-    $timeBlockDiv.append($hourDiv, $textArea, $button);
-    $button.append($saveIcon);
-    $container.append($timeBlockDiv);
-  }
+    let $textArea = Array.from($('.textarea'));
+    $textArea.forEach(element => {
+      if (parseInt(element.getAttribute("time-value")) < moment().hour()) {
+        element.classList.add("past");
+      } else if (parseInt(element.getAttribute("time-value")) > moment().hour()) {
+        element.classList.add("future");
+      } else {
+        element.classList.add("present");
+      }
+    });
+    
 });
